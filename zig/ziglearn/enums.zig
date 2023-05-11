7f108a07d57b75f735900bf8884023263164d2e0
Julien FRANCAIS <jfrancai@student.42.fr>
2023-05-11 11:35:27 +0200

    adding ziglings

---
+++ zig/ziglearn/enums.zig
@@ -0,0 +1,41 @@
+const expect = @import("std").testing.expect;
+
+const Direction = enum { north, south, east, west };
+
+const Value = enum(u2) { zero, one, two };
+
+test "enum ordinal value" {
+    try expect(@enumToInt(Value.zero) == 0);
+    try expect(@enumToInt(Value.one) == 1);
+    try expect(@enumToInt(Value.two) == 2);
+}
+
+const Value2 = enum(u32) {
+    hundred = 100,
+    thousand = 1000,
+    million = 1000000,
+    next,
+};
+
+test "set enum ordinal value" {
+    try expect(@enumToInt(Value2.hundred) == 100);
+    try expect(@enumToInt(Value2.thousand) == 1000);
+    try expect(@enumToInt(Value2.million) == 1000000);
+    try expect(@enumToInt(Value2.next) == 1000001);
+}
+
+const toto = *const fn (void) void;
+
+const Suit = enum {
+    clubs,
+    spades,
+    diamonds,
+    hearts,
+    fn isClubs(self: Suit) bool {
+        return self == Suit.clubs;
+    }
+};
+
+test "enum method" {
+    try expect(Suit.spades.isClubs() == Suit.isClubs(.spades));
+}
---
+++ zig/ziglearn/pointer.zig
@@ -0,0 +1,16 @@
+const expect = @import("std").testing.expect;
+
+fn increment(num: *u8) void {
+    num.* += 1;
+}
+
+test "pointers" {
+    var x: u8 = 1;
+    increment(&x);
+    try expect(x == 2);
+}
+
+test "usize" {
+    try expect(@sizeOf(usize) == @sizeOf(*u8));
+    try expect(@sizeOf(isize) == @sizeOf(*u8));
+}
