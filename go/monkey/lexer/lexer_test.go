be17a0ea151f38cec4ed70c8dafb5e3fc867fca1
Julien FRANCAIS <jfrancai@student.42.fr>
2023-05-14 22:18:26 +0200

    new test wip

--- go/monkey/lexer/lexer_test.go
+++ go/monkey/lexer/lexer_test.go
@@ -6,20 +6,44 @@ import (
 	"monkey/token"
 )
 
-func TestNextToke(t *testing.T) {
-	input := `=+(){},;`
+func TestNextToken(t *testing.T) {
+	input := `let five = 5;
+	let ten = 10;
+
+	let add = fn(x, y) {
+		x + y;
+	};
+
+	let result = add(five, ten);
+	`
 
 	tests := []struct {
 		expectedType token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
+		{token.IDENT, "x"},
 		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+
+
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
